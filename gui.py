e2c6885d45ef30431d8b864ef857c2901e4ad83f
Bastian <bastib83@gmail.com>
2019-11-19 23:36:12 +0100

    default

--- gui.py
+++ gui.py
@@ -2,6 +2,10 @@ from tkinter import *
 
 window = Tk()
 
-window.title("Basti´s PyGui")
+window.title("Basti´s PyGUI")
+
+lbl = Label(window, text="Yo Ko, bÄM!", font=("Arial Bold", 50))
+
+window.geometry('350x200')
 
 window.mainloop()
