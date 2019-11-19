a7c32bf034a57a0f0a6e2f95e8b39317acdaa68e
Bastian <bastib83@gmail.com>
2019-11-19 23:26:32 +0100

    default

--- gui.py
+++ gui.py
@@ -1,26 +1,7 @@
-from Tkinter import *
-import tkFont
+from tkinter import *
 
-win = Tk()
+window = Tk()
 
-myFont = tkFont.Font(family = 'Helvetica', size = 36, weight = 'bold')
+window.title("Basti´s PyGui")
 
-def printText():
-	print("LED button pressed")
-
-
-def exitProgram():
-	print("Exit Button pressed")
-	win.quit()
-
-
-win.title("First GUI")
-win.geometry('800x480')
-
-exitButton  = Button(win, text = "Exit", font = myFont, command = exitProgram, height =2 , width = 6)
-exitButton.pack(side = BOTTOM)
-
-ledButton = Button(win, text = "LED ON", font = myFont, command = printText, height = 2, width =8 )
-ledButton.pack()
-
-mainloop()
+window.mainloop()
